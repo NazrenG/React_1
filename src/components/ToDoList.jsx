8cfc44239af93ee5d67dc0d8d7b4f0b20573e793
Nəzrin Quliyeva <140939317+NazrenG@users.noreply.github.com>
2024-06-10 17:25:50 +0400

    Update ToDoList.jsx

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,38 +1,56 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const ToDoList = () => {
-  const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState("");
+class ToDoList extends Component {
+  state = {
+    tasks: [],
+    newTask: "",
+  };
 
-  const addTask = () => {
-    if (task.trim() !== "") {
-      setTasks([...tasks, task]);
-      setTask("");
+  addTask = () => {
+    const { newTask, tasks } = this.state;
+    if (newTask.trim()) {
+      this.setState({
+        tasks: [, newTask],
+        newTask: "",
+      });
     }
   };
 
-  const removeTask = (index) => {
-    const newTasks = tasks.filter((_, i) => i !== index);
-    setTasks(newTasks);
+  removeTask = (index) => {
+    const filterArr = this.state.tasks.filter((_, i) => i !== index);
+    this.setState({
+      tasks: filterArr,
+    });
   };
 
-  return (
-    <div>
-      <h1>ToDoList</h1>
+  handleInputChange = (event) => {
+    this.setState({ newTask: event.target.value });
+  };
+
+  render() {
+    const { tasks, newTask } = this.state;
+    return (
       <div>
-        <input type="text" value={task} />
-        <button onClick={addTask}>Əlavə et</button>
+        <h1>ToDoList</h1>
+        <div>
+          <input
+            type="text"
+            value={newTask}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.addTask}>Əlavə et</button>
+        </div>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button onClick={() => this.removeTask(index)}>Sil</button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
-            <button onClick={() => removeTask(index)}>Sil</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ToDoList;
